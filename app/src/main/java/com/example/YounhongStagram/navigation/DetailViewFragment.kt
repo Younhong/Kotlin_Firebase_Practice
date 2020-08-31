6b2d5e058c84b7b090ab025ccc663cb849e4045e
Younhong <younhong@kakao.com>
2020-08-31 17:48:14 +0900

    [UPDATE] design edit

--- app/src/main/java/com/example/YounhongStagram/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/YounhongStagram/navigation/DetailViewFragment.kt
@@ -5,15 +5,68 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.YounhongStagram.R
+import com.example.YounhongStagram.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_detail.view.*
+import kotlinx.android.synthetic.main.item_detail.view.*
 
 class DetailViewFragment : Fragment() {
+    var firestore : FirebaseFirestore? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
+        firestore = FirebaseFirestore.getInstance()
+
+        view.detailviewfragment_recyclerview.adapter = DetailViewRecyclerViewAdapter()
+        view.detailviewfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
         return view
+    }
+
+    inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var contentDTO : ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList : ArrayList<String> = arrayListOf()
+
+        init {
+            firestore?.collection("images")?.orderBy("timestamp")
+                ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    contentDTO.clear()
+                    contentUidList.clear()
+                    for (snapshot in querySnapshot!!.documents) {
+                        var item = snapshot.toObject(ContentDTO::class.java)
+                        contentDTO.add(item!!)
+                        contentUidList.add(snapshot.id)
+                    }
+                    notifyDataSetChanged()
+                }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_detail, parent, false)
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var viewholder = (holder as CustomViewHolder).itemView
+
+            viewholder.detailviewitem_profile_textview.text = contentDTO!![position].userId
+            Glide.with(holder.itemView.context).load(contentDTO!![position].imageUrl).into(viewholder.detailviewitem_imageview_content)
+            viewholder.detailviewitem_explain_textview.text = contentDTO!![position].explain
+            viewholder.detailviewitem_favorite_counter_textview.text = "Likes " + contentDTO!![position].favoriteCount
+            Glide.with(holder.itemView.context).load(contentDTO!![position].imageUrl).into(viewholder.detailviewitem_profile_image)
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTO.size
+        }
+
     }
 }
