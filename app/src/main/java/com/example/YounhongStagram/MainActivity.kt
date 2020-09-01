bb3ce93fde8172c0da5ef2b8f556639aaaca51c1
Younhong <younhong@kakao.com>
2020-09-01 12:19:12 +0900

    [UPDATE] 푸쉬 알림 설정

--- app/src/main/java/com/example/YounhongStagram/MainActivity.kt
+++ app/src/main/java/com/example/YounhongStagram/MainActivity.kt
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             1)
 
         bottom_navigation.selectedItemId = R.id.action_home
+        registerPushToken()
     }
 
     override fun onNavigationItemSelected(p0: MenuItem) : Boolean {
@@ -80,6 +82,18 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         toolbar_username?.visibility = View.GONE
         toolbar_btn_back.visibility = View.GONE
         toolbar_title_image?.visibility = View.VISIBLE
+    }
+
+    fun registerPushToken() {
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener { task ->
+            val token = task.result?.token
+            val uid = FirebaseAuth.getInstance().currentUser?.uid
+            val map = mutableMapOf<String, Any>()
+            map["pushToken"] = token!!
+
+            FirebaseFirestore.getInstance().collection("pushtokens")
+                .document(uid!!).set(map)
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
